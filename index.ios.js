09ec82d49dcb7c0a7422cc5f2523bc3252696a08
Ricardo Cardoso <rick.duk@gmail.com>
2017-06-19 13:01:51 -0300

    Added image in project

--- index.ios.js
+++ index.ios.js
@@ -1,6 +1,6 @@
 //Imports
 import React from 'react';
-import { Text, AppRegistry, View, TouchableOpacity } from 'react-native';
+import { Text, AppRegistry, View, Image } from 'react-native';
 
 //Styles
 const Styles = {
@@ -32,22 +32,22 @@ const Styles = {
   	backgroundColor : 'cornflowerblue',
   	paddingTop : 40
   },
-  button : {
-  	backgroundColor : '#48BBEC',
-  	padding : 10,
-  	borderColor : '#1D8EB8',
-  	borderWidth : 1,
-  	borderRadius : 8,
-  	shadowColor : '#000',
-  	shadowOffset : { width:0, height: 2},
-  	shadowOpacity : 0.4
-  },
-  textButton : {
-  	color : '#FFF',
-  	fontSize : 16, 
-  	fontWeight : 'bold',
-  	alignSelf : 'center'
-  }
+	// button : {
+	// 	backgroundColor : '#48BBEC',
+	// 	padding : 10,
+	// 	borderColor : '#1D8EB8',
+	// 	borderWidth : 1,
+	// 	borderRadius : 8,
+	// 	shadowColor : '#000',
+	// 	shadowOffset : { width:0, height: 2},
+	// 	shadowOpacity : 0.4
+	// },
+	// textButton : {
+	// 	color : '#FFF',
+	// 	fontSize : 16, 
+	// 	fontWeight : 'bold',
+	// 	alignSelf : 'center'
+	// }
 //   top : {
 //   	flex : 2, // 2:11
 //   	backgroundColor : 'brown'
@@ -60,20 +60,29 @@ const Styles = {
 //   	flex : 1, // 1:11
 //   	backgroundColor : 'orangered'
 //   }
+	imageStyle : {
+		justifyContent : 'flex-end',
+		padding : 5
+	}
 }; 
 
-const pressButton = () => {
-	alert('Botão Pressionado');
-};
+// const pressButton = () => {
+// 	alert('Botão Pressionado');
+// };
 
 //Components
 const App = () => {
-	const { main, button, textButton } = Styles;
+	const { main, imageStyle } = Styles;
   return (
   	<View style={ main }>
-    	<TouchableOpacity style={ button }>
-    		<Text style={ textButton }>Clique aqui</Text>
-    	</TouchableOpacity>
+    	// <TouchableOpacity style={ button }>
+    	// 	<Text style={ textButton }>Clique aqui</Text>
+    	// </TouchableOpacity>
+    	<Image 
+    		source={ require('./imgs/uvas.png') }
+    		style= { imageStyle } >
+    		<Text> Legenda para imagem </Text>
+    	</Image>
     </View>
   );
 };
